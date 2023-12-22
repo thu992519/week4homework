7df4a6394a08dec4e9d611ed90efc96d24fb84f6
RX00 <thu992519@gmail.com>
2023-12-23 02:03:40 +0800

    Updates

---
+++ assets/serise-5ae0605c.js
@@ -0,0 +1,256 @@
+import"./main-3c25ca17.js";console.log("serise");let a=document.querySelectorAll(".ser-nav");a.forEach(e=>{e.addEventListener("click",l=>{if(a.forEach(c=>{c.classList.remove("active")}),e.classList.add("active"),l.target.classList[3]=="sunglass"){let c=document.querySelector(".serpic");c.innerHTML=`  <div class="imgleft2"></div>
+            <div class="imgright2"></div>`;let s="",i="",t=document.querySelector(".ser-title"),d=document.querySelector(".ser-content");s=`<p class="mb-0">SUN GLASS</p>
+            <p>太陽眼鏡</p>`,t.innerHTML=s,i=`<div class="row justify-content-center ">
+            <div class="col-md-4 col-6">
+              <img src="../assets/img/product-7.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-4 col-6">
+              <img src="../assets/img/product-8.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-4 col-6">
+              <img src="../assets/img/product-9.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            
+          </div>
+          <div class="row justify-content-center mt-1">
+            <div class="col-md-4 col-6">
+              <img src="../assets/img/product-5.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-4 col-6">
+              <img src="../assets/img/product-6.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-4 col-6">
+              <img src="../assets/img/product-7.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            
+          </div>
+          <div class="row justify-content-center mt-1 mb-3">
+            <div class="col-md-4 col-6">
+              <img src="../assets/img/product-9.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-4 col-6">
+              <img src="../assets/img/product-10.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-4 col-6">
+              <img src="../assets/img/product-11.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+           
+          </div>`,d.innerHTML=i}if(l.target.classList[3]=="opacial"){let c=document.querySelector(".serpic");c.innerHTML=`  <div class="imgleft"></div>
+            <div class="imgright"></div>`;let s="",i="",t=document.querySelector(".ser-title"),d=document.querySelector(".ser-content");s=`<p class="mb-0">Celluloid Combi</p>
+            <p>賽璐鉻鈦金屬混合框</p>`,t.innerHTML=s,i=`<div class="row justify-content-center ">
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-1.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-2.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-3.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-4.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+          </div>
+          <div class="row justify-content-center mt-1">
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-5.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-6.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-7.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-8.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+          </div>
+          <div class="row justify-content-center mt-1 mb-3">
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-9.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-10.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-11.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-12.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+          </div>`,d.innerHTML=i}if(l.target.classList[3]=="functional"){let c=document.querySelector(".serpic");c.innerHTML=`  <div class="imgleft"></div>
+            <div class="imgright2"></div>`;let s="",i="",t=document.querySelector(".ser-title"),d=document.querySelector(".ser-content");s=`<p class="mb-0">functional</p>
+            <p>機能性眼鏡</p>`,t.innerHTML=s,i=`<div class="row justify-content-center ">
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-12.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-10.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-4.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-5.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+          </div>
+          <div class="row justify-content-center mt-1">
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-8.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-9.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-7.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-8.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+          </div>
+          <div class="row justify-content-center mt-1 mb-3">
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-9.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-10.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-11.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+            <div class="col-md-3 col-6">
+              <img src="../assets/img/product-12.png" alt="">
+              <div class="d-flex justify-content-between">
+                <div class="left">BJ41600S</div>
+                <div class="right"><span class="price">NTD3460</span></div>
+              </div>
+            </div>
+          </div>`,d.innerHTML=i}})});
