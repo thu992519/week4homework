19c52bfb94079e10c5b73eeaf3e5d686dd675834
thu992519 <thu992519@gmail.com>
2023-12-25 09:59:35 +0800

    header edit

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-axios.get('/assets/php/login.php')
-  .then(response => {
-    // 处理成功的响应
-    console.log(response.data);
-    console.log("123");
-  })
-  .catch(error => {
-    // 处理错误
-    console.log("123");
+// axios.get('/assets/php/login.php')
+//   .then(response => {
+//     // 处理成功的响应
+//     console.log(response.data);
+//     console.log("123");
+//   })
+//   .catch(error => {
+//     // 处理错误
+//     console.log("123");
     
-  });
+//   });
