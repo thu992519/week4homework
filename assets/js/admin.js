61a124dc004ca9d19a461384be5582c52eef9939
thu992519 <thu992519@gmail.com>
2024-01-02 09:24:20 +0800

    member finsh

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -8,6 +8,8 @@ let edit_send=document.querySelector("#m_update_btn");
 
 
 
+
+
 let list=[]; 
 
 navlink.forEach((item)=>{
@@ -62,7 +64,7 @@ function showdata(list){
        <td data-th="修改/刪除">
            <button class="btn btn-success edit"  data-bs-toggle="modal" data-bs-target="#updateModal"
            data-id="${item.ID}"  data-name="${item.name}" data-city="${item.city}" data-time="${item.lastlogindatetime}">修改</button>
-           <button class="btn btn-danger" data-id="${item.ID}" id="del">刪除</button>
+           <button class="btn btn-danger del" data-id="${item.ID}" id="del">刪除</button>
        </td>
    </tr>`;
     })
@@ -83,6 +85,9 @@ function init(){
       
         showdata(list);
         editshow();
+        creatshow();
+        deltodo();
+        
     })
 }
 
@@ -91,20 +96,20 @@ function editshow(){
     let edit_button=document.querySelectorAll(".edit");
     edit_button.forEach((item)=>{
         item.addEventListener("click",function(){
-
-            document.querySelector('#m_pname').value=item.getAttribute('data-name');
-            document.querySelector('#m_city').value=item.getAttribute('data-city');  
-
+            let m_pname=document.querySelector('#m_pname');
+            m_pname.value=item.getAttribute('data-name');
+            let m_city=document.querySelector('#m_city');
+            m_city.value=item.getAttribute('data-city');
             edit_send.addEventListener("click",function(){
-                console.log(document.querySelector('#m_pname').value);
-                axios.put('http://127.0.0.1/topicphp/admin/update.php', { m_id:item.getAttribute('data-id'),m_pname: item.getAttribute('data-name') ,m_city:item.getAttribute('data-city')},{ headers: {
+                console.log( item.getAttribute('data-name'));
+                axios.post('http://127.0.0.1/topicphp/admin/update.php', { m_id:item.getAttribute('data-id'),m_pname: m_pname.value ,m_city:m_city.value},{ headers: {
                   'Content-Type': 'application/json',
                   'admintest': 'admintest' // 這裡的值可以是任何你想要傳遞的字串
                 },})
                   .then(response => {
                     console.log(response);
-                    // location.reload();
-                
+                    location.reload();
+                    
                   })
                   .catch(error => {
                     console.log(error);
@@ -117,3 +122,55 @@ function editshow(){
     })
 }
 
+
+//按下新增
+function creatshow(){
+    newbtn.addEventListener('click',function(){
+        let new_pname=document.querySelector('#m_new_pname');
+        let new_city=document.querySelector('#m_new_city');
+        axios.post('http://127.0.0.1/topicphp/admin/creatadmin.php', { m_pname: new_pname.value ,m_city:new_city.value},{ headers: {
+                  'Content-Type': 'application/json',
+                  'admintest': 'admintest' // 這裡的值可以是任何你想要傳遞的字串
+                },})
+                  .then(response => {
+                    console.log(response);
+                    console.log(response.data);
+                    location.reload();
+                    
+                  })
+                  .catch(error => {
+                    console.log(error);
+                   
+                  });
+    })
+
+
+   
+}
+
+//按下刪除
+function deltodo(){
+    let del_btn=document.querySelectorAll(".del");
+    del_btn.forEach((item)=>{
+        item.addEventListener('click',function(){
+            console.log(item);
+            console.log(item.getAttribute('data-id'));
+            axios.post('http://127.0.0.1/topicphp/admin/delmember.php', { m_id:item.getAttribute('data-id') },{ headers: {
+                  'Content-Type': 'application/json',
+                  'admintest': 'admintest' 
+                },})
+                  .then(response => {
+                    console.log(response);
+                    console.log(response.data);
+                    location.reload();
+                    
+                  })
+                  .catch(error => {
+                    console.log(error);
+                   
+                  });
+
+        });
+    })
+   
+}
